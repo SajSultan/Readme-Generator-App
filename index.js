9bd62df43e202cb7347bc07071d04ce1cff07efc
SajSultan <ksa.sultan1979@yahoo.com>
2023-02-27 23:56:19 +0000

    commit after first readme created successfully

--- index.js
+++ index.js
@@ -1,66 +1,80 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-//Inquirer questions for user.
-inquirer.prompt(
-    [
-        {
-            type: "input",
-            message: "What is the title of your project?",
-            name: "title"
-        },
-        // {
-        //     type: "input",
-        //     message: "Describe your project app?",
-        //     name: "description"
-        // },
-        // {
-        //     type: "input",
-        //     message: "How do you install the project?",
-        //     name: "installation"
-        // },
-        // {
-        //     type: "input",
-        //     message: "How to report issues for your project?",
-        //     name: "reporting-issues"
-        // },
-        // {
-        //     type: "input",
-        //     message: "How to make contributions for your project?",
-        //     name: "contribution-info"
-        // },
-        // {
-        //     type: "input",
-        //     message: "How to make contributions for your project?",
-        //     name: "contribution-info"
-        // },
-        // {
-
-        // }    
-    ]
-)
-.then((response) => {
-    console.log(response);
-    const readmeTemplate = generateMarkdown(response)
-    fs.writeFile(
-        `${response.title}.md`,
-        readmeTemplate
-        ,
-         (error) => {
-      return error
-        ? console.error(error)
-        : console.log("Write File operation Success!");
+//Inquirer questions for project creator.
+// function to initialize program
+function init(questions) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message:
+          "Provide a short description explaining the what, why, and how of your project?",
+        name: "description",
+      },
+      {
+        type: "input",
+        message:
+          "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.?",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "How to make contributions for your project?",
+        name: "contribution",
+      },
+      {
+        type: "input",
+        message:
+          "Provide instructions and examples for use with screenshots if possible?",
+        name: "usage",
+      },
+      {
+        type: "list",
+        message: "License?",
+        name: "license",
+        choices: ["MIT", "ISC", "GNUPLv3", "Apache License 2.0", "Boost Software License"],
+      },
+      {
+        type: "input",
+        message: "Have questions for the project?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the linkedin url?",
+        name: "linkedin",
+      },
+      {
+        type: "input",
+        message: "What is the github url?",
+        name: "github",
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      const readmeTemplate = generateMarkdown(response);
+      fs.writeFile("README.md", readmeTemplate, (error) => {
+        return error
+          ? console.error(error)
+          : console.log("Readme has been generated successfully!");
+      })
     });
+}
+init();
+// function to write to README file
+function writeToFile(fileName) {
+  fs.writeFile(`README.md`, readmeTemplate, (error) => {
+    return error
+      ? console.error(error)
+      : console.log("Readme has been generated successfully!");
   });
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// // function to initialize program
-// function init() {
-
-// }
-
+}
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(answers) {
-  return `# ${answers.title}
+  return `# ${answers.title.toUpperCase()}
+  ## Badges
+  ${answers.license}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contributing](#contributing)
+  
+  ## Installation
+  ${answers.installation}
+  ## Usage
+  ${answers.usage}
 
-`;
+
+  ## License
+  ${answers.license}
+  
+ 
+  
+  ## Contributing
+  ${answers.contribution}
+
+
+  # Contact
+  * Github :${answers.github}
+  * LinkedIn :${answers.linkedin}
+  * Email :${answers.email}
+
+  
+`
 }
-
 module.exports = generateMarkdown;
