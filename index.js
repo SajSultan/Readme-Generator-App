2a30a41408dd025ecad2dfe59cf10ae8912b81ac
SajSultan <ksa.sultan1979@yahoo.com>
2023-02-27 17:58:15 +0000

    set up questions and write file

--- index.js
+++ index.js
@@ -11,42 +11,49 @@ inquirer.prompt(
             message: "What is the title of your project?",
             name: "title"
         },
-        {
-            type: "input",
-            message: "Describe your project app?",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "How do you use the project?",
-            name: "usage"
-        },
-        {
-            type: "input",
-            message: "How do you install the project?",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "How to report issues for your project?",
-            name: "reporting-issues"
-        },
-        {
-            type: "input",
-            message: "How to make contributions for your project?",
-            name: "contribution-info"
-        },
-        {
-            type: "input",
-            message: "How to make contributions for your project?",
-            name: "contribution-info"
-        },    
+        // {
+        //     type: "input",
+        //     message: "Describe your project app?",
+        //     name: "description"
+        // },
+        // {
+        //     type: "input",
+        //     message: "How do you install the project?",
+        //     name: "installation"
+        // },
+        // {
+        //     type: "input",
+        //     message: "How to report issues for your project?",
+        //     name: "reporting-issues"
+        // },
+        // {
+        //     type: "input",
+        //     message: "How to make contributions for your project?",
+        //     name: "contribution-info"
+        // },
+        // {
+        //     type: "input",
+        //     message: "How to make contributions for your project?",
+        //     name: "contribution-info"
+        // },
+        // {
+
+        // }    
     ]
 )
 .then((response) => {
     console.log(response);
     const readmeTemplate = generateMarkdown(response)
-})
+    fs.writeFile(
+        `${response.title}.md`,
+        readmeTemplate
+        ,
+         (error) => {
+      return error
+        ? console.error(error)
+        : console.log("Write File operation Success!");
+    });
+  });
 // // function to write README file
 // function writeToFile(fileName, data) {
 // }
@@ -56,5 +63,4 @@ inquirer.prompt(
 
 // }
 
-// // // function call to initialize program
-// // init();
+
